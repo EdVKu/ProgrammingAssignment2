62fcb9fcd9860b22345d38f370b0a82755a7c8a1
vkurieduardo@gmail.com <mail@ewenharrison.com>
2020-04-16 20:47:21 -0500

    First commit (test)

--- cachematrix.R
+++ cachematrix.R
@@ -3,8 +3,40 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+makeVector <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
+}
 
+cachemean <- function(x, ...) {
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- mean(data, ...)
+  x$setmean(m)
+  m
+}
+
+
+makeCacheMatrix <- function(x = matrix()) {
+  if(length(x)==4)
+    n <<- x
+  ## the n object is declared to be used outside of the function, in order for it to be cached by other 
+  ## functions
+  else
+    break
 }
 
 
